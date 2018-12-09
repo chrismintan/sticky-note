6ceae1a30acbcd19b49b4543866eb832d0421133
chrismintan <chris_acs@hotmail.com>
2018-12-09 20:54:48 +0800

    added color palette + able to add new sticky note via cloning element

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+const palette = [
+  "#FFFF88",
+  "#A1F094",
+  "#FFABE4",
+  "#FDCC65FF",
+  "#D4B1FE",
+  "#9FDEFD",
+  "#E0E1DD"
+];
+
 window.onload = function() {
   console.log('linked!')
-}
-
-function addNote() {
-  console.log("Add note")
 }
 
 function deleteAll() {
@@ -12,4 +18,17 @@ function deleteAll() {
 
 function search() {
   console.log('search code here')
+}
+
+function newSticky() {
+  // Cloning hidden sticky
+  let template = document.getElementById('template');
+  let newSticky = template.cloneNode(true);
+  newSticky.style.display = "inline-block";
+  newSticky.style.background = palette.green;
+
+  // Appending sticky to board
+  let board = document.getElementById('board');
+  board.appendChild(newSticky);
+  console.log('appended!')
 }
