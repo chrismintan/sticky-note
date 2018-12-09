d05427e33909ea6bbfc7334d9ea2d754398dd519
chrismintan <chris_acs@hotmail.com>
2018-12-09 21:17:48 +0800

    added options bar that hides/shows when moused over

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const palette = [
 
 window.onload = function() {
   console.log('linked!')
+  persistentNotes();
 }
 
 function deleteAll() {
@@ -20,7 +21,33 @@ function search() {
   console.log('search code here')
 }
 
+function persistentNotes() {
+  if ( localStorage.hasOwnProperty('chrisiscool') ) {
+    let data = localStorage.getItem('chrisiscool');
+    notes = JSON.parse(data);
+  } else {
+    notes = ["mynameischris!"];
+  }
+
+  // Clearing up empty notes
+  for ( let i = 0; i < notes.length; i++ ) {
+    if ( notes[i] == "" ) {
+      notes.splice(i,1);
+    }
+  }
+
+  // Appending notes to board
+  for ( let i = 0; i < notes.length; i++ ) {
+    if ( notes[i] != "mynameischris!" && notes[i] != null ) {
+      newSticky(notes[i].header, notes[i].content, notes[i].color);
+    }
+  }
+}
+
 function newSticky() {
+  // Just shortening script
+  let pos = parseInt(document.getElementsByClassName("note").length);
+
   // Cloning hidden sticky
   let template = document.getElementById('template');
   let newSticky = template.cloneNode(true);
@@ -31,4 +58,11 @@ function newSticky() {
   let board = document.getElementById('board');
   board.appendChild(newSticky);
   console.log('appended!')
+
+  // Making options visible when hovering
+  let optionsArr = document.getElementsByClassName("options");
+  function show() { optionsArr[pos].style.visibility = "visible" };
+  function hide() { optionsArr[pos].style.visibility = "hidden" };
+  newSticky.addEventListener("mouseover", show)
+  newSticky.addEventListener("mouseout", hide)
 }
